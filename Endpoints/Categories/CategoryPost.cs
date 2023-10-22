853f3dc2f101722e8cbc37f70a4d21de1aac696f
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-10-21 22:17:34 -0300

    Creating CategoryPost

---
+++ Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,27 @@
+using product_orders.Domain.Products;
+using product_orders.Infrastructure.Data;
+
+namespace product_orders.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        var category = new Category
+        {
+            Name = categoryRequest.Name,
+            CreatedBy = "Test",
+            CreatedOn = DateTime.Now,
+            EditedBy = "Test",
+            EditedOn = DateTime.Now
+        };
+        context.Categories.Add(category);
+        context.SaveChanges();
+
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
+}
