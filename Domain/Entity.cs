d6af1bc823578234cd19dccdbc2670af5c1b1409
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-10-21 22:15:58 -0300

    Creating entity

---
+++ Domain/Entity.cs
@@ -0,0 +1,16 @@
+namespace product_orders.Domain;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string CreatedBy { get; set; } = null!;
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; } = null!;
+    public DateTime EditedOn { get; set; }
+
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+}
