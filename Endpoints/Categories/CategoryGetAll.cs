98277396261da632bcdbafb08bfd57c8697c31e3
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-10-21 22:17:55 -0300

    Creating CategoryGetAll

---
+++ Endpoints/Categories/CategoryGetAll.cs
@@ -0,0 +1,19 @@
+using product_orders.Infrastructure.Data;
+
+namespace product_orders.Endpoints.Categories;
+
+public class CategoryGetAll
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(ApplicationDbContext context)
+    {
+        var categories = context.Categories.ToList();
+        var response = categories.Select(
+            c => new CategoryResponse { Id = c.Id, Name = c.Name, Active = c.Active }
+        );
+        return Results.Ok(response);
+    }
+}
