95ceb37bcf0450818798786eebc88e89bf355850
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-10-22 00:02:49 -0300

    Validating CategoryPut data

--- Endpoints/Categories/CategoryPut.cs
+++ Endpoints/Categories/CategoryPut.cs
@@ -9,15 +9,15 @@ public class CategoryPut
     public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action([FromRoute]Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
+    public static IResult Action([FromRoute] Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
-        
         if (category == null)
             return Results.NotFound();
 
-        category.Name = categoryRequest.Name;
-        category.Active = categoryRequest.Active;
+        category.EditInfo(categoryRequest.Name, categoryRequest.Active);
+        if (!category.IsValid)
+            return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
 
         context.SaveChanges();
         return Results.Ok();
