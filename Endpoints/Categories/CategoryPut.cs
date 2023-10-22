092f286ace9be162b3a87de5609f79e114643f47
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-10-21 23:30:16 -0300

    Validating resource not found

--- Endpoints/Categories/CategoryPut.cs
+++ Endpoints/Categories/CategoryPut.cs
@@ -13,6 +13,10 @@ public class CategoryPut
     public static IResult Action([FromRoute]Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        
+        if (category == null)
+            return Results.NotFound();
+
         category.Name = categoryRequest.Name;
         category.Active = categoryRequest.Active;
 
